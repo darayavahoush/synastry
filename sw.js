30ec1c79f91d6cf9815bca15fec52ecdef37688e
Ananya Sridhar <ananyasridhar108@gmail.com>
2026-09-21 23:45:06 +0530

    OneSignal app id + tab shortcuts

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 try{importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js")}catch(e){}
-const C="syn-v7",F=["./","./index.html","./data.js","./manifest.webmanifest","./icon.png","./icon-192.png"];
+const C="syn-v8",F=["./","./index.html","./data.js","./manifest.webmanifest","./icon.png","./icon-192.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)).catch(()=>{}))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))))});
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET"||/onesignal\.com/.test(e.request.url))return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{
     const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp).catch(()=>{}));return res;
   }).catch(()=>caches.match("./index.html"))));
 });
