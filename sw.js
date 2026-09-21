70ebe875b92fa727fd959a7dba425f6d29886bf5
Ananya Sridhar <ananyasridhar108@gmail.com>
2026-09-21 23:34:12 +0530

    OneSignal v16 push + daily notification

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const C="syn-v6",F=["./","./index.html","./data.js","./manifest.webmanifest","./icon.png","./icon-192.png"];
+try{importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js")}catch(e){}
+const C="syn-v7",F=["./","./index.html","./data.js","./manifest.webmanifest","./icon.png","./icon-192.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)).catch(()=>{}))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))))});
 self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
+  if(e.request.method!=="GET"||/onesignal\.com/.test(e.request.url))return;
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
     const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp).catch(()=>{}));return res;
   }).catch(()=>caches.match("./index.html"))));
