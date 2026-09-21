97a370a8888317e13330a5d1b0b5d151e2b26648
Ananya Sridhar <ananyasridhar108@gmail.com>
2026-09-21 23:31:25 +0530

    magical zodiac icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="syn-v5",F=["./","./index.html","./data.js","./manifest.webmanifest","./icon.png"];
+const C="syn-v6",F=["./","./index.html","./data.js","./manifest.webmanifest","./icon.png","./icon-192.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)).catch(()=>{}))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))))});
 self.addEventListener("fetch",e=>{
